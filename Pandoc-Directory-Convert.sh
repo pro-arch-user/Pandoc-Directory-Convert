3a9df757c35ec42cf05845b92519bb28035bb08f
Max <74821331+pro-arch-user@users.noreply.github.com>
2024-09-20 21:26:22 +0300

    Add "fromPandoc",

--- Pandoc-Directory-Convert.sh
+++ Pandoc-Directory-Convert.sh
@@ -1,23 +1,25 @@
 # args:
 # initial_path - directory to be converted
-# from - extension of files that you want to convert from DOESN'T WORK WITH PANDOC EXTENSIONS
-# to - extension of files that you want to convert to IN PANDOC
-# delete - to delete the files used that you converted from. Make sure to backup your files before you do this.
+# from - extension of files that you want to convert from
+# fromPandoc - extension of files that you want to convert from in pandoc
+# toPandoc - extension of files that you want to convert to in pandoc
+# delete - (optional) to delete the files used that you converted from. Make sure to backup your files before you do this.
 parse_dir () {
 	initial_path=$1
 	from=$2
-	to=$3
-	delete=$4
+ 	$fromPandoc = $3
+	toPandoc=$4
+	delete=$5
 
 	echo Converting directory $initial_path
 	for file_path in "$initial_path"/*; do
 		if [ -d "$file_path" ]; then
-			parse_dir "$file_path" $from $to $delete
+			parse_dir "$file_path" $from $fromPandoc $toPandoc $delete
 		elif [[ $file_path == *.$from ]]; then
 			file_name=$(basename "$file_path")
 			file_path_no_file_extension=${file_path%.*}
 
-			pandoc "$file_path" -f $from -t $to -o "$file_path_no_file_extension.md"
+			pandoc "$file_path" -f $fromPandoc -t $toPandoc -o "$file_path_no_file_extension.md"
 
 			if [[ $delete == "delete" ]]; then
 				rm -f "$file_path"
@@ -27,13 +29,13 @@ parse_dir () {
 	echo Finished converting directory $initial_path
 }
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Minimum 3 parameters are required: "directory", "from" and "to""
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+  echo "Minimum 3 parameters are required: "directory", "from", "fromPandoc" and "toPandoc""
   exit 1
 fi
 
 echo Convertion started
 echo
-parse_dir "$1" $2 $3 $4
+parse_dir "$1" $2 $3 $4 $5
 echo
 echo Convertion finished
